5a24bf35504171c2e0b1962f6c89245b4c4f9315
qaws7791 <qaws7791@gmail.com>
2023-05-28 23:33:03 +0900

    feat: 디렉터리 구조 변경, 검색 페이지, 장르 페이지 생성

---
+++ scripts/fetch.js
@@ -0,0 +1,44 @@
+import env from "./env.json" assert { type: "json" };
+const ApiOptions = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${env.accessToken}`,
+  },
+};
+
+export async function fetchPopularMovies() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+    ApiOptions
+  );
+  const { results } = await res.json();
+  return results;
+}
+
+export async function fetchMovieDetail(id) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?language=ko-KR`,
+    ApiOptions
+  );
+  const json = await res.json();
+  return json;
+}
+
+export async function fetchGenreMovies(genreId) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc&with_genres=${genreId}`,
+    ApiOptions
+  );
+  const { results } = await res.json();
+  return results;
+}
+
+export async function fetchSearchMovies(query) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`,
+    ApiOptions
+  );
+  const { results } = await res.json();
+  return results;
+}
